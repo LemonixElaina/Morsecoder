84ad4866fdc7fc8ff591eac9ed3c40d2efb56666
Lemonix-xxx <84227290+Lemonix-xxx@users.noreply.github.com>
2021-10-03 11:06:50 +0800

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,15 +1,28 @@
+# 导入
+
 from morsecoder import Morsecoder
 
-morse1 = Morsecoder(text='Hello World', sep='/')
-print(f'The text of morse1: {morse1.text}')
-for i in morse1.morse_en():
-    print(i, end='')
+# 编码演示
+
+my_code = Morsecoder(text='Hello World', sep='/')
+for value in my_code.get_encode():
+    print(value, end='')
+print()
+print(my_code.to_string('get_encode'))
+
+# 译码演示
+
+my_code.set_args(text='...././.-../.-../---/ /.--/---/.-./.-../-../',
+                 sep=my_code.get_args()['sep']
+                 )
+for value in my_code.get_decode():
+    print(value, end='')
 print()
 
-Morsecoder.modify('×', '.----...-.')
+# __str__
 
-morse2 = Morsecoder(text='.----...-.', sep=' ')
-print(f'The text of morse2: {morse2.text}')
-for i in morse2.morse_de():
-    print(i, end='')
-print()
+print(my_code)
+
+# Doc
+
+print(help(Morsecoder))
